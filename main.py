cbbd40e4ab5a4f10e81ec84bccc250caf3288ac0
Leonid Astrin <astrinleonid@gmail.com>
2022-12-08 17:53:18 +0200

    Commit 22 pre_submission

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ def parse_sections(soup,limit = -1, prof_area_param = '',db_mode = 'checkonly', 
                 period_ID = db.table_update_row_return_id('periods', 'text', period, {'text' : period})
 
                 record = {'state_ID' : state_ID, 'period_id' : period_ID, **values}
-                db.table_add_row('stats_data', record)
+                db.table_add_row('stat_data', record)
 
     db.sql_exec("SELECT * FROM agencies LIMIT 3", 's')
     db.sql_exec("SELECT * FROM departments LIMIT 3", 's')
